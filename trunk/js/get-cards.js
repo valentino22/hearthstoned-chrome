3a367c1e968f136490bc844a9a1f8b86ca2d6615
szabi22@gmail.com <szabi22@gmail.com@d1c3b543-e48f-a383-d4f3-39c056876006>
2014-01-21 17:57:01 +0000

    Tablesorter added.

--- trunk/js/get-cards.js
+++ trunk/js/get-cards.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": true }
 	$('#hsdeck-content table').filterTable();
 	$(".filter-table input").focus();
+	
+	$('table#table-card-list').tablesorter({
+		sortReset      : true,
+		theme: 'blackice',
+	});
 });
